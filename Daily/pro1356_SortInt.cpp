c264726aea488278255729c02c4068f654d74f49
Malus-domestica <fshafique57@gmail.com>
2023-11-01 01:43:18 +0530

    adding more code

---
+++ Daily/pro1356_SortInt.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+    static int numberof1s(int a){
+        int count = 0;
+        while(a){
+            count += a&1;
+            a = a>>1;
+        }
+        return count;
+    }
+    static bool compare(int a, int b){
+        int p = numberof1s(a);
+        int q = numberof1s(b);
+        if(p==q){
+            return (a<=b);
+        }
+        else{
+            return (p<q);
+        }
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),compare);
+        return arr;
+    }
+};
